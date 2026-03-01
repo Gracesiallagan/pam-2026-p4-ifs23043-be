743b55576fdd2973b42390526bc30a32f9e449b7
Gracesiallagan <siallagangrace6@gmail.com>
2026-03-01 13:11:24 +0700

    fixx

--- data.sql
+++ data.sql
@@ -8,3 +8,15 @@ CREATE TABLE IF NOT EXISTS plants (
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS drinks (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    nama VARCHAR(100) NOT NULL,
+    path_gambar VARCHAR(255) NOT NULL,
+    deskripsi TEXT NOT NULL,
+    bahan_utama TEXT NOT NULL,
+    cara_penyajian TEXT NOT NULL,
+    manfaat TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+    );
